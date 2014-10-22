45a362d76bcdf58294b5cd120a676424a989a27a
Danilo Barbato <dbarbato@tv1.com.br>
2014-10-22 13:41:36 -0200

    Fix x position of village life bar

--- js/class/Village.js
+++ js/class/Village.js
@@ -14,13 +14,14 @@ var Village = function(sprite, health) {
     this.village.lifeBar = game.add.graphics(0, 0); // inicia o retangulo
     this.village.lifeBar.lineStyle(2, 0x000000, 1); // largura, cor, alfa
     this.village.lifeBar.beginFill(0xFFFF00, 0.8) // cor, alfa
-    this.village.lifeBar.drawRect(this.village.x-this.village.width/2, this.village.y-this.village.height/2, this.village.width, 5); // x, y, largura, altura
+    this.village.lifeBar.drawRect(this.village.x, this.village.y-this.village.height/2, this.village.width, 5); // x, y, largura, altura
 
     // Desenha um retângulo de vida sobre a vila
     this.village.lifeBarStatus = game.add.graphics(0, 0); // inicia o retangulo
     this.village.lifeBarStatus.lineStyle(2, 0x000000, 0); // largura, cor, alfa
     this.village.lifeBarStatus.beginFill(0x00FF00, 0.9) // cor, alfa
-    this.village.lifeBarStatus.drawRect(this.village.x-this.village.width/2, this.village.y-this.village.height/2, this.village.width, 5); // x, y, largura, altura
+    this.village.lifeBarStatus.drawRect(this.village.x, this.village.y-this.village.height/2, this.village.width, 5); // x, y, largura, altura
+    this.village.lifeBarStatus.enableBody = true;
 
 }
 
@@ -31,14 +32,9 @@ Village.prototype.damageTaken = function(village, monster) {
     monster.lifeBarStatus.destroy();
     monster.lifeBar.destroy();
 
-    // Atualiza a barra de vida
-    village.lifeBar.x = village.x-village.width;
-    village.lifeBar.y = village.y-village.height/2;
     // Calcula o dano tomado - escala e posiciona a barra de vida atual
-    damTaken = village.health/village.totalHealth;
+    damTaken = monster.health/monster.totalHealth;
     village.lifeBarStatus.scale.x = damTaken;
-    // Atualiza posicao da barra de vida atual
-    village.lifeBarStatus.y = village.y-village.height/2;
     village.lifeBarStatus.x += (1-(village.health/village.totalHealth))*village.width/2;
 
     if (village.health <= 0) {
@@ -49,6 +45,8 @@ Village.prototype.damageTaken = function(village, monster) {
 Village.prototype.death = function(village) {
     // TODO - condicao de derrota
     if (village.health <= 0) {
+        village.lifeBar.destroy();
+        village.lifeBarStatus.destroy();
         village.kill();
     }
     //this.game.state.start('death_state');
